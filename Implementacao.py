f267e7e3028f4cb2c42d970a6a89356da35bbd41
Paola <paolaleonardiruvinski@gmail.com>
2026-04-09 12:32:01 -0300

    Update Implementacao.py

--- Implementacao.py
+++ Implementacao.py
@@ -10,3 +10,27 @@ for i in range (1,101):
         print (i)
         numero += 1
 print ("Quantidade de números é:", numero)
+
+print ("Exercício 02)
+
+#Peça ao usuário um número inteiro positivo n. Não permita que o programa continue
+#caso o número não seja válido. Em seguida, calcule e exiba a soma de todos os números
+#de 1 até n. Ao final exiba a expressão aritmética completa, incluindo o resultado.
+#Exemplo: n = 5 -> Output: 1 + 2 + 3 + 4 + 5 = 15
+
+N = int (input ("Diga um número inteiro positivo \n> "))
+while N <0:
+    print ("Número inválido")
+    N = int (input ("Diga um número inteiro positivo \n> "))
+
+total = 0
+espaco= ""
+
+for i in range (1, N+1):
+    total += i
+
+    if i == 1:
+        espaco += str(i) #Trasnformo p/ string
+    else:
+        espaco += "+" + str(i)
+print (f"{espaco} = {total}")
