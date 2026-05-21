e6279484b80cfb511d00aa7590653b159055ede2
Paola <paolaleonardiruvinski@gmail.com>
2026-05-21 11:59:31 -0300

    Add function definitions for various tasks

---
+++ Raciocínio_Algorítmico_Funcoes.py
@@ -0,0 +1,50 @@
+
+#1- Escreva uma função chamada "imprimir_nome" que imprime o seu nome.
+#def imprimir_nome(nome):
+#    return nome
+#nome = input("Diga seu nome")
+
+#print (imprimir_nome(nome))
+
+#2 - Escreva uma função chamada "maior" que receba três números como parâmetros e
+# retorne o maior entre eles.
+
+#def maior(x, y, z):
+#    if x <= y and z <= y:
+#        return y
+#    elif y <= x and z <= x:
+#        return x
+#    else:
+#        return z
+
+#x = int(input("Diga um número"))
+#y = int(input("Diga outro número"))
+#z = int(input("Diga o último número"))
+
+#print ("O maior número é:", maior(x, y, z))
+
+#3 - Escreva uma função chamada "criar_vetor" que retorna um vetor preenchido com zeros
+#de tamanho 5.
+#vetor = [0,0,0,0,0]
+
+#def criar_vetor():
+#    return vetor
+
+#print (criar_vetor())
+
+#4 - Escreva uma função chamada "media" que receba uma lista de números
+# como parâmetro e retorne a média desses números.
+
+#def media(lista):
+#    return sum(lista) / len(lista)
+
+#um = int(input("Diga um valor"))
+#dois = int(input("Mais outro valor"))
+#tres = int(input("último valor"))
+
+#lista=[um,dois,tres]
+
+#print (media(lista))
+
+#5 - Escreva uma função chamada "inverter" que receba uma string
+#como parâmetro e imprime a string invertida.
