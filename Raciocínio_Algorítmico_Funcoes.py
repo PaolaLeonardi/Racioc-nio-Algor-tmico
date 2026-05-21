32167ec33da0bc712e8598de5d7a164554fa32d9
Paola <paolaleonardiruvinski@gmail.com>
2026-05-21 15:27:07 -0300

    Refactor functions and improve print statements

--- Raciocínio_Algorítmico_Funcoes.py
+++ Raciocínio_Algorítmico_Funcoes.py
@@ -1,53 +1,71 @@
 
-#1- Escreva uma função chamada "imprimir_nome" que imprime o seu nome.
-#def imprimir_nome(nome):
-#    return nome
-#nome = input("Diga seu nome")
+1- Escreva uma função chamada "imprimir_nome" que imprime o seu nome.
+def imprimir_nome(nome):
+    return nome
+nome = input("Diga seu nome")
 
-#print (imprimir_nome(nome))
+print (imprimir_nome(nome))
 
-#2 - Escreva uma função chamada "maior" que receba três números como parâmetros e
-# retorne o maior entre eles.
+2 - Escreva uma função chamada "maior" que receba três números como parâmetros e
+ retorne o maior entre eles.
 
-#def maior(x, y, z):
-#    if x <= y and z <= y:
-#        return y
-#    elif y <= x and z <= x:
-#        return x
-#    else:
-#        return z
+def maior(x, y, z):
+    if x <= y and z <= y:
+        return y
+    elif y <= x and z <= x:
+        return x
+    else:
+        return z
 
-#x = int(input("Diga um número"))
-#y = int(input("Diga outro número"))
-#z = int(input("Diga o último número"))
+x = int(input("Diga um número"))
+y = int(input("Diga outro número"))
+z = int(input("Diga o último número"))
 
-#print ("O maior número é:", maior(x, y, z))
+print ("O maior número é:", maior(x, y, z))
 
-#3 - Escreva uma função chamada "criar_vetor" que retorna um vetor preenchido com zeros
-#de tamanho 5.
-#vetor = [0,0,0,0,0]
+3 - Escreva uma função chamada "criar_vetor" que retorna um vetor preenchido com zeros
+de tamanho 5.
+vetor = [0,0,0,0,0]
 
-#def criar_vetor():
-#    return vetor
+def criar_vetor():
+    return vetor
 
-#print (criar_vetor())
+print (criar_vetor())
 
-#4 - Escreva uma função chamada "media" que receba uma lista de números
-# como parâmetro e retorne a média desses números.
+4 - Escreva uma função chamada "media" que receba uma lista de números
+ como parâmetro e retorne a média desses números.
 
-#def media(lista):
-#    return sum(lista) / len(lista)
+def media(lista):
+    return sum(lista) / len(lista)
 
-#um = int(input("Diga um valor"))
-#dois = int(input("Mais outro valor"))
-#tres = int(input("último valor"))
+um = int(input("Diga um valor"))
+dois = int(input("Mais outro valor"))
+tres = int(input("último valor"))
 
-#lista=[um,dois,tres]
+lista=[um,dois,tres]
 
-#print (media(lista))
+print (media(lista))
 
-#5 - Escreva uma função chamada "inverter" que receba uma string
-#como parâmetro e imprime a string invertida.
+5 - Escreva uma função chamada "inverter" que receba uma string
+como parâmetro e imprime a string invertida.
 
-#6- Escreva uma função chamada "imprime_diagonal" que recebe uma matriz de tamanho
-#3x3 preenchida com valores quaisquer, e imprime os valores na diagonal principal.
+def inverter(string):
+    return string[::-1] #significa “pegar a string de trás para frente”
+
+valor = input("Diga uma palavra")
+
+print (inverter(valor))
+
+6- Escreva uma função chamada "imprime_diagonal" que recebe uma matriz de tamanho
+3x3 preenchida com valores quaisquer, e imprime os valores na diagonal principal.
+
+matriz = [
+    [2,0,0,],
+    [0,2,0,],
+    [0,0,2,]
+    ]
+
+def imprime_diagonal(matriz):
+    return matriz [0][0], matriz[1][1],matriz[2][2]
+
+print (imprime_diagonal(matriz))
