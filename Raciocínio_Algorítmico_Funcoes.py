f8b6e5a89ff13b538156e5c4a83b3755f246d492
Paola <paolaleonardiruvinski@gmail.com>
2026-05-22 11:09:59 -0300

    Refactor functions into main and nested structure

--- Raciocínio_Algorítmico_Funcoes.py
+++ Raciocínio_Algorítmico_Funcoes.py
@@ -1,197 +1,202 @@
-#A)
-#1- Escreva uma função chamada "imprimir_nome" que imprime o seu nome.
-def imprimir_nome(nome):
-    return nome
-nome = input("Diga seu nome")
+def main():
 
-print (imprimir_nome(nome))
+    # A)
 
-#2 - Escreva uma função chamada "maior" que receba três números como parâmetros e
-# retorne o maior entre eles.
+    def um():
+        def imprimir_nome(nome):
+            return nome
 
-def maior(x, y, z):
-    if x <= y and z <= y:
-        return y
-    elif y <= x and z <= x:
-        return x
-    else:
-        return z
+        nome = input("Diga seu nome: ")
+        print(imprimir_nome(nome))
 
-x = int(input("Diga um número"))
-y = int(input("Diga outro número"))
-z = int(input("Diga o último número"))
 
-print ("O maior número é:", maior(x, y, z))
+    def dois():
+        def maior(x, y, z):
+            if x <= y and z <= y:
+                return y
+            elif y <= x and z <= x:
+                return x
+            else:
+                return z
 
-#3 - Escreva uma função chamada "criar_vetor" que retorna um vetor preenchido com zeros
-#de tamanho 5.
-vetor = [0,0,0,0,0]
+        x = int(input("Diga um número: "))
+        y = int(input("Diga outro número: "))
+        z = int(input("Diga o último número: "))
 
-def criar_vetor():
-    return vetor
+        print("O maior número é:", maior(x, y, z))
 
-print (criar_vetor())
 
-#4 - Escreva uma função chamada "media" que receba uma lista de números
-# como parâmetro e retorne a média desses números.
+    def tres():
 
-def media(lista):
-    return sum(lista) / len(lista)
+        def criar_vetor():
+            return [0, 0, 0, 0, 0]
 
-um = int(input("Diga um valor"))
-dois = int(input("Mais outro valor"))
-tres = int(input("último valor"))
+        print(criar_vetor())
 
-lista=[um,dois,tres]
 
-print (media(lista))
+    def quatro():
 
-#5 - Escreva uma função chamada "inverter" que receba uma string
-#como parâmetro e imprime a string invertida.
+        def media(lista):
+            return sum(lista) / len(lista)
 
-def inverter(string):
-    return string[::-1] #significa “pegar a string de trás para frente”
+        n1 = int(input("Diga um valor: "))
+        n2 = int(input("Mais outro valor: "))
+        n3 = int(input("Último valor: "))
 
-valor = input("Diga uma palavra")
+        lista = [n1, n2, n3]
 
-print (inverter(valor))
+        print(media(lista))
 
-#6- Escreva uma função chamada "imprime_diagonal" que recebe uma matriz de tamanho
-#3x3 preenchida com valores quaisquer, e imprime os valores na diagonal principal.
 
-matriz = [
-    [2,0,0,],
-    [0,2,0,],
-    [0,0,2,]
-    ]
+    def cinco():
 
-def imprime_diagonal(matriz):
-    return matriz [0][0], matriz[1][1],matriz[2][2]
+        def inverter(string):
+            return string[::-1]
 
-print (imprime_diagonal(matriz))
+        valor = input("Diga uma palavra: ")
 
-#B) 
-#1 - Crie uma função chamada soma_elementos que receba uma lista de
-#números como parâmetro e retorne a soma de todos os elementos dessa lista.
+        print(inverter(valor))
 
-lista=[]
-for i in range(5):
-    
-    valor = int(input("Diga um número:"))
-    lista.append(valor)
 
-def soma_elementos(lista):
-    return sum(lista)
+    def seis():
 
-print(soma_elementos(lista))
+        matriz = [
+            [2, 0, 0],
+            [0, 2, 0],
+            [0, 0, 2]
+        ]
 
-#2- Crie uma função chamada e palindromo que receba uma string como
-#parâmetro e retorne True se a string for um palíndromo (ou seja, se lida de trás
-#para frente for igual à original) e False caso contrário.
+        def imprime_diagonal(matriz):
+            return matriz[0][0], matriz[1][1], matriz[2][2]
 
-def e_palindromo(string):
-    if string == string[::-1]:
-        return  True
-    else:
-        return  False
+        print(imprime_diagonal(matriz))
 
-palavra = input("Digite uma palavra:")
 
-print(e_palindromo(palavra))
+    # B)
 
-#3- Crie uma função chamada maior_elemento que receba uma lista de números
-#como parâmetro e retorne o maior elemento dessa lista.
+    def umB():
 
-def maior_elemento(lista):
-    maior = lista[0]
+        lista = []
 
-    for numero in lista:
-        if numero > maior:
-            maior = numero
+        for i in range(5):
+            valor = int(input("Diga um número: "))
+            lista.append(valor)
 
-    return maior
+        def soma_elementos(lista):
+            return sum(lista)
 
-#4- Crie uma função chamada contar_caracteres que receba uma string e um
-#caractere como parâmetros e retorne o número de vezes que o caractere
-#aparece na string
+        print(soma_elementos(lista))
 
-def contar_caracteres(string, caractere):
-    return string.count(caractere)
 
-texto = input("Digite uma palavra: ")
-caractere = input("Digite um caractere: ")
+    def doisB():
 
-print(contar_caracteres(texto, caractere))
+        def e_palindromo(string):
+            return string == string[::-1]
 
-#5- Implemente uma calculadora simples em Python utilizando funções. A
-#calculadora deve ser capaz de realizar as seguintes operações
-#matemáticas básicas:
-#• Soma
-#• Subtração
-#• Multiplicação
-#• Divisão
-#Requisitos:
-#• Crie uma função para cada operação matemática (soma,
-#subtração, multiplicação e divisão). As funções devem receber
-#dois valores e retornar o resultado da operação.
-#• Implemente uma função para exibir o menu de opções para o
-#usuário.
-#• O programa deve repetir o menu após cada operação, até que
-#o usuário escolha a opção de sai
+        palavra = input("Digite uma palavra: ")
 
-x = 0
-y = 0
+        print(e_palindromo(palavra))
 
-def somar(x,y):
-    return x + y
 
-def subtrair(x,y):
-    return x - y
+    def tresB():
 
-def multiplicar(x,y):
-    return x * y
+        def maior_elemento(lista):
 
-def dividir(x,y):
-    if y == 0:
-        return "Erro: divisão por zero!"
-    else:
-        return x / y
+            maior = lista[0]
 
-def menu():
-    print("\n=== CALCULADORA ===")
-    print("1 - Somar")
-    print("2 - Subtrair")
-    print("3 - Multiplicar")
-    print("4 - Dividir")
-    print("5 - Sair")
+            for numero in lista:
+                if numero > maior:
+                    maior = numero
 
-while True:
+            return maior
 
-    menu()
+        lista = [1, 5, 9, 2, 7]
 
-    opcao = str(input("Selecione uma opção: ").lower())
+        print(maior_elemento(lista))
 
-    if opcao == "5":
-        print("Encerrando a calculadora...")
-        break
 
-    x = int(input("Qual o primeiro número: "))
-    y = int(input("Qual o segundo número: "))
+    def quatroB():
 
-    if opcao == "somar":
-        resultado = somar(x, y)
+        def contar_caracteres(string, caractere):
+            return string.count(caractere)
 
-    elif opcao == "subtrair":
-        resultado = subtrair(x, y)
+        texto = input("Digite uma palavra: ")
+        caractere = input("Digite um caractere: ")
 
-    elif opcao == "multiplicar":
-        resultado = multiplicar(x, y)
+        print(contar_caracteres(texto, caractere))
 
-    elif opcao == "dividir":
-        resultado = dividir(x, y)
 
-    else:
-        print("Opção invalida")
-        continue
+    def cincoB():
 
-    print(f"Você escolheu {opcao}, sua resposta é: {resultado}")
+        def somar(x, y):
+            return x + y
+
+        def subtrair(x, y):
+            return x - y
+
+        def multiplicar(x, y):
+            return x * y
+
+        def dividir(x, y):
+            if y == 0:
+                return "Erro: divisão por zero!"
+            else:
+                return x / y
+
+        def menu():
+            print("\n=== CALCULADORA ===")
+            print("1 - Somar")
+            print("2 - Subtrair")
+            print("3 - Multiplicar")
+            print("4 - Dividir")
+            print("5 - Sair")
+
+        while True:
+
+            menu()
+
+            opcao = input("Selecione uma opção: ")
+
+            if opcao == "5":
+                print("Encerrando a calculadora...")
+                break
+
+            x = int(input("Qual o primeiro número: "))
+            y = int(input("Qual o segundo número: "))
+
+            if opcao == "1":
+                resultado = somar(x, y)
+
+            elif opcao == "2":
+                resultado = subtrair(x, y)
+
+            elif opcao == "3":
+                resultado = multiplicar(x, y)
+
+            elif opcao == "4":
+                resultado = dividir(x, y)
+
+            else:
+                print("Opção inválida")
+                continue
+
+            print(f"Resultado: {resultado}")
+
+
+    # Chamando as funções
+
+    um()
+    dois()
+    tres()
+    quatro()
+    cinco()
+    seis()
+
+    umB()
+    doisB()
+    tresB()
+    quatroB()
+    cincoB()
+
+
+main()
