6c852fc04e8f4d6f84a2e0a930ffea74877346ef
Paola <paolaleonardiruvinski@gmail.com>
2026-05-22 11:18:09 -0300

    Add initial implementation for matrix operations

---
+++ Raciocinio_Algoritmico_Matrizes.py
@@ -0,0 +1,19 @@
+#1- Declare uma matriz 5 x 5. Preencha com 1 a diagonal principal e com 0 os 
+#demais elementos. Escreva ao final a matriz obtida.
+
+#2. Leia uma matriz 4 x 4, imprima a matriz e retorne a localização (linha e a
+#coluna) do maior valor.
+
+#3. Faça um programa que leia uma matriz de 5 linhas e 4 colunas contendo as
+#seguintes informações sobre alunos de uma disciplina, sendo todas as
+#informações do tipo inteiro:
+#a. Primeira coluna: número de matrícula (use um inteiro)
+#b. Segunda coluna: media das provas
+#c. Terceira coluna: media dos trabalhos
+#d. Quarta coluna: nota final
+  # Elabore um programa que:
+  #a. Leia as três primeiras informações de cada aluno;
+  #b. Calcule a nota final como sendo a soma da média das provas e da
+  #média dos trabalhos;
+  #c. Imprima a matrícula do aluno que obteve a maior nota final (assuma que só existe
+  #uma maior nota)
